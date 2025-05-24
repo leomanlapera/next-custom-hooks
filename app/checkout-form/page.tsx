e9f683b551b52f9a7f7d76ed5e0546eaab885196
June Leoman Lapera <leomanlapera@gmail.com>
2025-05-24 22:06:38 +0800

    day 16

--- app/checkout-form/page.tsx
+++ app/checkout-form/page.tsx
@@ -1,5 +1,123 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  name: z
+    .string()
+    .min(2, { message: "Name must contain at least 2 character(s)" }),
+  email: z.string().email(),
+  address: z
+    .string()
+    .min(2, { message: "Address must contain at least 2 character(s)" }),
+  city: z.string(),
+  zip: z.string(),
+});
+
+type TFormDataSchema = z.infer<typeof formSchema>;
+
 export default function CheckoutForm() {
-  return <main className="max-w-lg mx-auto p-8">Checkout Form</main>;
+  const form = useForm<TFormDataSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      address: "",
+      city: "",
+      zip: "",
+    },
+  });
+
+  function onSubmit(values: TFormDataSchema) {
+    console.log(values);
+  }
+
+  return (
+    <main className="max-w-lg mx-auto p-8">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="John Doe" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="johndoe@domain.com" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="address"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Address</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Purok Caimito, Awayan Street"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="city"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>City</FormLabel>
+                <FormControl>
+                  <Input placeholder="Carcar City" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="zip"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Zip</FormLabel>
+                <FormControl>
+                  <Input placeholder="6000" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </main>
+  );
 }
